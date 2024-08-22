b29b7e6c6875e15e7f80c8e80f8bde808a2ecf7e
Steve <swemhtet.net@gmail.com>
2024-08-22 21:36:41 +0700

    added comment

--- routes/testing.route.js
+++ routes/testing.route.js
@@ -4,5 +4,8 @@ import { testing } from "../controllers/testing.controller.js";
 const router = express.Router();
 
 router.post("/", testing);
+/**
+ * chit tl ko gyi doh
+ */
 
 export default router;
