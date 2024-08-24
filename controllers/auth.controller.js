f016b4fd23dbf85a8889323960701c29c64d0bc8
ATPaing <aungthetpaing.2001@gmail.com>
2024-08-25 06:16:09 +0700

    add change/reset password function and remove password field in response  when user login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -90,12 +90,77 @@ export const login = async (req, res) => {
         //this is to create a token
         const token = genToken(toEncrypt);
 
+        // destructure the user object to remove the password field
+        const { password: _ , ...userInfo } = user.toObject();
+        
         //this is to send the response and token to the client-frontend and save in local storage
-        fMsg(res, "Login Successfully", { user, token }, 200);
+        fMsg(res, "Login Successfully", { userInfo, token }, 200);
     } catch (error) {
         fMsg(res, "Login failed", error.message, 500);
     }
 };
+
+export const passwordReset = async (req, res) => {
+
+    try {
+        const { email, newPassword } = req.body;
+
+        if (!email.trim() || !newPassword.trim()) {
+            return fMsg(res, "Password reset failed", "Email and new password are required", 400);
+        }
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return fMsg(res, "Password reset failed", "User not found", 404);
+        }
+
+        const hashedPassword = encode(newPassword);
+
+        await User.updateOne({ email }, { password: hashedPassword });
+
+        fMsg(res, "Password reset successful", null, 204 );
+    } catch (error) {
+        fMsg(res, "Password reset failed", error.message, 500);
+    }
+}
+
+export const changePassword = async (req, res) => {
+    try {
+        // console.log(req.user)
+        const { email } = req.user;
+        console.log(email)
+        const { oldPassword, newPassword, confirmedNewPassword } = req.body;
+
+        if (!email.trim() || !oldPassword.trim() || !newPassword.trim() || !confirmedNewPassword.trim()) {
+            return fMsg(res, "Password change failed", "Email, old password, new password and confirmed new password are required", 400);
+        }
+
+        const user = await User.findOne({ email });
+        console.log(user)
+        if (!user) {
+            return fMsg(res, "Password change failed", "User not found", 404);
+        }
+        // console.log(user.password)
+        const isMatch = decode(oldPassword, user.password);
+        console.log(oldPassword)
+        console.log(isMatch)
+        if (!isMatch) {
+            return fMsg(res, "Password change failed", "Invalid old password", 400);
+        }
+
+        if (newPassword !== confirmedNewPassword) {
+            return fMsg(res, "Password change failed", "New password and confirmed new password do not match", 400);
+        }
+
+        const hashedPassword = encode(newPassword);
+
+        await User.updateOne({ email }, { password: hashedPassword });
+
+        fMsg(res, "Password change successful", null, 204 );
+    } catch (error) {
+        fMsg(res, "Password change failed", error.message, 500);
+    }
+}
 
 //note
 //acutually we dont have to generate token for both login and register, it may depend on the workflow of UI but discuss later ..But i have created both but will delete one base on discussion
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,10 @@
-import { register,login } from "../controllers/auth.controller.js";
+import { register, login, passwordReset, changePassword } from "../controllers/auth.controller.js";
+import { validateToken } from "../utils/validator.js";
 import express from "express";
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.post("/passwordReset", passwordReset);
+router.post("/changePassword", validateToken(), changePassword);
 export default router;
