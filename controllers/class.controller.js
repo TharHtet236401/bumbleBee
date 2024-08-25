da56d51a17a50e5764b8269994670739b4dfb82f
Steve <swemhtet.net@gmail.com>
2024-08-25 10:27:01 +0700

    some progress to class controller before pulling

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -1,14 +1,18 @@
 import { fMsg } from "../utils/libby.js";
 import User from "../models/user.model.js";
+import School from "../models/school.model.js";
 import { generateClassCode } from "../utils/libby.js";
 
 export const createClass = async(req, res) => {
     try{
         const { grade, className } = req.body;
         const user = await User.findById(req.user._id);
-        // const school = user.schools;
+        // const school = user.schools[0];
+        const school = await School.findById(user.schools[0]);
+        let classLists = [];
+        classLists = school.classes;
 
-        console.log("this is user's school " + JSON.stringify(user));
+        console.log("this is user's school " + classLists.length);
 
         //generate the class code with 4 digits.
         const code = generateClassCode(4);
