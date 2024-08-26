e0a303609278e8eb6559b0f99f1007307edc9f2e
TharHtet236401 <tharhteta3@gmail.com>
2024-08-26 23:15:33 +0700

    draft of the createRequest.

--- controllers/request.controller.js
+++ controllers/request.controller.js
@@ -1,5 +1,6 @@
 import PendingRequest from "../models/pendingRequest.model.js";
 import Class from "../models/class.model.js";
+import Student from "../models/student.model.js";
 import User from "../models/user.model.js";
 import { fMsg } from "../utils/libby.js";
 
@@ -8,18 +9,24 @@ export const createRequest = async (req, res) => {  // When the guardian and the
         const { classCode ,student_id } = req.body;
         const userId = req.user._id;
 
-        if(req.user.roles.includes("guardian")){
-            const guardian = await User.findById(userId)
-            if(!guardian.childern.includes(student_id)){
-                guardian.childern.push(student_id)
-                await guardian.save()
-            }
-        }
+        
 
         // Find the desired class using the provided class code
         const desireClass = await Class.findOne({ classCode });
         if (!desireClass) {
             return fMsg(res, "Class not found", "The class with the given code does not exist", 404);
+        }
+
+        if(req.user.roles.includes("guardian")){
+            const guardian = await User.findById(userId)
+        
+            if(!guardian.childern.includes(student_id)){
+                guardian.childern.push(student_id)
+              
+                guardian.classes.push(desireClass._id)
+                await guardian.save()
+                
+            }
         }
 
         // Check if the user already has a pending request for this class
