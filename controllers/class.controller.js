c93cbcfaaa2236290a1f72594e38f460a8e88f11
TharHtet236401 <tharhteta3@gmail.com>
2024-08-27 16:40:19 +0700

    integrate the function readClassByAdmin

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -138,11 +138,38 @@ export const deleteClass = async(req, res) => {
     }
 }
 
-export const readClass = async(req, res) => {
-    try{
-        ConstantSourceNode.l
-    }catch(err){
-        console.log(err)
-        fMsg(res, "error in reading the class", null,err)
+export const readClassByAdmin = async (req, res) => { // differenet admins can read different schools classes
+    try {
+        // Find the user and populate the school data if it's required
+        const user = await User.findById(req.user._id);
+        if (!user) {
+            return fMsg(res, "User not found", null, 404);
+        }
+
+        // Assuming user.schools is an array and we need to handle multiple schools in the future
+        const schoolId = user.schools[0];
+        if (!schoolId) {
+            return fMsg(res, "No school associated with this user", null, 400);
+        }
+
+        
+        const [school, classes] = await Promise.all([
+            School.findById(schoolId),
+            Class.find({ school: schoolId })
+        ]);
+
+        if (!school) {
+            return fMsg(res, "School not found", null, 404);
+        }
+
+        if (!classes || classes.length === 0) {
+            return fMsg(res, "No classes found for this school", [], 200);
+        }
+
+        fMsg(res, "Classes found", classes, 200);
+
+    } catch (err) {
+        console.error("Error in reading the class:", err);
+        fMsg(res, "Error in reading the class", null, err);
     }
-}
+};
