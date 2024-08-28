5931825d537a5a78666f6f96a7790eab28f3dc70
TharHtet236401 <tharhteta3@gmail.com>
2024-08-28 10:09:28 +0700

    added two functions to retrieve the students

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -11,7 +11,7 @@ export const addStudentToClass = async (req, res) => {
         if (student) {
             // Check if class_id is already in student's classes array
 
-            student.classes.push(class_id);
+            student.classes.push(class_id);g
 
             await student.save();
         } else {
@@ -36,3 +36,34 @@ export const addStudentToClass = async (req, res) => {
         fMsg(res, "Error in creating or updating student", error, 500);
     }
 };
+
+export const getStudentsByClass = async (req, res) => {
+    try {
+        const class_id = req.params.class_id;
+        const classObj = await Class.findById(class_id).populate('students'); // Populate students
+
+        // const students = await Student.find({ classes: class_id });We can also search like
+        if (!classObj) {
+            return fMsg(res, "Class not found", null, 404); // Return if class not found
+        }
+
+        fMsg(res, "Students fetched successfully", classObj.students, 200); // Return populated students
+    } catch (error) {
+        fMsg(res, "Error in fetching students", error, 500);
+    }
+};
+
+
+export const getStudentsByClassCode = async (req, res) => {
+    try {
+        const classCode = req.params.classCode;
+        const classObj = await Class.findOne({ classCode }).populate('students');
+        
+        if (!classObj) {
+            return fMsg(res, "Class not found", null, 404);
+        }
+        fMsg(res, "Students fetched successfully", classObj.students, 200);
+    } catch (error) {
+        fMsg(res, "Error in fetching students", error, 500);
+    }
+}
--- routes/student.route.js
+++ routes/student.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addStudentToClass } from "../controllers/student.controller.js";
+import { addStudentToClass, getStudentsByClass, getStudentsByClassCode } from "../controllers/student.controller.js";
 import { isTeacher, validateToken } from "../utils/validator.js";
 
 const router = express.Router();
 
 router.post("/add/:class_id", validateToken(), isTeacher(), addStudentToClass);
-
+router.get("/get/:class_id", validateToken(), isTeacher(), getStudentsByClass);
+router.get("/getByClassCode/:classCode", getStudentsByClassCode);
 export default router;
