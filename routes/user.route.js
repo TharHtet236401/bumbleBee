b2e859967172ecd4575f4ffa178a08ba03058e1f
ATPaing <aungthetpaing.2001@gmail.com>
2024-09-01 06:10:59 +0700

    users can now edit the profile picture

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,4 +1,10 @@
 import express from "express";
+import multer from 'multer';
+
+import { UserSchema } from "../utils/schema.js";
+import { validateBody } from "../utils/validator.js";
+
+import { parse } from 'path';
 
 import {
     validateToken,
@@ -11,9 +17,44 @@ import {
     getAllUsers
 } from "../controllers/user.controller.js";
 
+import User from "../models/user.model.js";
+
 const router = express.Router();
 
-router.put("/update", validateToken(), updateUserInfo);
+// Middleware to fetch user email
+const fetchUserEmail = async (req, res, next) => {
+    if (!req.body.email) {
+        try {
+            const user = await User.findById(req.user._id);
+            if (user) {
+                req.email = user.email;
+            } else {
+                return res.status(404).send('User not found');
+            }
+        } catch (error) {
+            return res.status(500).send('Error fetching user email');
+        }
+    } else if (req.body.email) {
+        req.email = req.body.email;
+    }
+    next(); // Proceed only if everything is fine
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+
+        cb(null, 'uploads/profile_pictures');
+    },
+    filename: (req, file, cb) => {
+        const { ext } = parse(file.originalname);
+
+        cb(null, `${req.email}${ext}`); // Ensure unique file names
+    }
+});
+
+const upload = multer({ storage });
+
+router.put("/update", validateToken(), fetchUserEmail, upload.single('profilePicture'), validateBody(UserSchema.edit), updateUserInfo);
 
 router.post("/delete/:userId", deleteUser);
 
