1b7ad8330f55737b63b823842f4833b6167e3f7b
Steve <swemhtet.net@gmail.com>
2024-09-01 21:50:55 +0700

    fix test routes lol

--- routes/test.route.js
+++ routes/test.route.js
@@ -1,10 +1,18 @@
 import express from "express";
-import {fMsg} from "../utils/libby";
+import { fMsg } from "../utils/libby.js";
 
 const router = express.Router();
-router.get("/getApi", fMsg(res, "You have get response", {response: "get"}, 200))
-router.post("/postApi", fMsg(res, "You have post response", {response: "post"}, 200))
-router.put("/putApi", fMsg(res, "You have put response", {response: "put"}, 200))
-router.delete("/deleteApi", fMsg(res, "You have delete response", {response: "delete"}, 200))
+router.get("/getApi", (req, res) => {
+    fMsg(res, "You have get response", {"response": "get"}, 200)
+})
+router.post("/postApi", (req, res) => {
+    fMsg(res, "You have post response", {"response": "post"}, 200)
+})
+router.put("/putApi", (req, res) => {
+    fMsg(res, "You have put response", {"response": "put"}, 200)
+})
+router.delete("/deleteApi", (req, res) => {
+    fMsg(res, "You have delete response", {"response": "delete"}, 200)
+})
 
 export default router;
