ea89329f10e72abb203bbe62ae14991dc7e59b75
ATPaing <aungthetpaing.2001@gmail.com>
2024-09-08 17:04:23 +0700

    refactored the user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,55 +1,60 @@
 import User from "../models/user.model.js";
 import { fileURLToPath } from "url";
-import { fMsg, paginate } from "../utils/libby.js";
+import { fMsg, paginate, deleteFile } from "../utils/libby.js";
 import fs from "fs";
 import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const updateUserInfo = async (req, res) => {
+export const updateUserInfo = async (req, res, next) => {
     try {
         const userId = req.user._id;
         const userInfo = await User.findById(userId);
         const username = req.body.userName;
         const oldPath = path.join(__dirname, "..", userInfo.profilePicture);
 
+
         if (username && (req.file == undefined || req.file == null)) {
-
-
-            fs.access(oldPath, fs.constants.F_OK, (err) => {
-                if (!err) {
-                    // user has a profile picture only username is updated
-                    // do nothing
-                } 
-                else {
-                    // user does not have a profile picture
-                    // generate a new profile picture with dicebear
-                    console.log('ee')
-                    const encodedUsername = encodeURIComponent(username);
-                    req.body.profilePicture = `https://api.dicebear.com/9.x/initials/svg?seed=${encodedUsername}`;
-                }
-            });
-
-        } else if (req.file) {
-            // user has a profile picture and is updating it
-            // replace it with new one
+            try {
+                await fs.promises.access(oldPath, fs.constants.F_OK);
+                // user has a profile picture, only username is updated
+                // do nothing
+                console.log(`${oldPath} exists`);
+                deleteFile(oldPath);
+                const encodedUsername = encodeURIComponent(username);
+                req.body.profilePicture = `https://api.dicebear.com/9.x/initials/svg?seed=${encodedUsername}`;
+            } catch (err) {
+                // user does not have a profile picture
+                console.log(`${oldPath} does not exist`);
+                const encodedUsername = encodeURIComponent(username);
+                req.body.profilePicture = `https://api.dicebear.com/9.x/initials/svg?seed=${encodedUsername}`;
+                console.log(req.body.profilePicture);
+            }
+        } else if (!username && (req.file == undefined || req.file == null)) {
+            const user = await User.findById(userId);
+            const encodedUsername = encodeURIComponent(user.userName);
+            req.body.profilePicture = `https://api.dicebear.com/9.x/initials/svg?seed=${encodedUsername}`
+        }
+        else if ( req.file != undefined || req.file != null) {
+            // user is updating profile picture
             req.body.profilePicture = `/uploads/profile_pictures/${req.file.filename}`;
-        } 
+        }
 
         const user = await User.findByIdAndUpdate(
             userId,
-            {
-                ...req.body,
-            },
+            { ...req.body },
             { new: true }
         );
+
+        console.log(user);
         fMsg(res, "User updated successfully", user, 200);
     } catch (error) {
         console.log(error);
-        fMsg(res, "error in updating user", error, 500);
+        next(error);
     }
 };
+
 
 export const deleteUser = async (req, res) => {
     try {
@@ -58,7 +63,7 @@ export const deleteUser = async (req, res) => {
         fMsg(res, "User deleted successfully", user, 200);
     } catch (error) {
         console.log(error);
-        fMsg(res, "error in deleting user", error, 500);
+        next(error);
     }
 };
 
@@ -86,6 +91,6 @@ export const getAllUsers = async (req, res) => {
         // Return paginated response
         fMsg(res, "Users fetched successfully", paginatedData, 200);
     } catch (error) {
-        fMsg(res, "Error in fetching users", error, 500);
+        next(error);
     }
 };
