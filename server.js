4605c3921690f7678b7c83f46203ded6dbf9cc22
ATPaing <aungthetpaing.2001@gmail.com>
2024-09-30 04:05:36 +0100

    cors_fixed

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
+const server = http.createServer(app);
+const io = new Server(server);
 
 
 app.use(cors({
