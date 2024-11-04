ae62ad237bf66c3e43ba4915bb4b2dd54495c309
TharHtet236401 <tharhteta3@gmail.com>
2024-11-05 03:47:11 +0700

    testing with header authorization successs

--- config/connectMongoDb.js
+++ config/connectMongoDb.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL;
+const MONGO_DB_URL = process.env.MONGO_DB;
 const connectToMongoDB = async () => {
   try {
 
