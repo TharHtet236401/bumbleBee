7da6bb3a63d2aa58874ba4e1c7d8e32bb78fdba7
TharHtet236401 <tharhteta3@gmail.com>
2024-11-30 04:13:05 +0700

    Update leaveRequest.route.js to remove guardian restriction on reading all leave requests

--- routes/leaveRequest.route.js
+++ routes/leaveRequest.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/create", validateToken(), isGuardian(), createLeaveRequest);
 
 router.get("/readByClass/:classId", validateToken(), isTeacher(), readLeaveRequestByClass);
-router.get("/readAllReq", validateToken(), isGuardian(), readAllLeaveRequests);
+router.get("/readAllReq", validateToken(), readAllLeaveRequests);
 
 router.put("/edit", validateToken(), isGuardian(), editLeaveRequest);
 
